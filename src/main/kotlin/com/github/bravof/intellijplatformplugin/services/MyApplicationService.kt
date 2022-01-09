02f4b56983c9cd4e887622cadd14813ef01d4150
GitHub Action <action@github.com>
2022-01-09 14:43:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/bravof/intellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bravof.intellijplatformplugin.services
+
+import com.github.bravof.intellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
